1d8637bb51a09193e48a48d734c80a5c1e0b6414
xingyu.liao <sherlockliao01@gmail.com>
2023-07-23 22:40:28 +0800

    fix url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
+  website: "https://sherlock-dev.netlify.app/",
   author: "Sherlock Liao",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "Distill",
